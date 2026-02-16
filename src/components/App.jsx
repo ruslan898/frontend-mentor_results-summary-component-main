247da7ba41ef47586ec0ca087fc7adcb4aa5f45c
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-02-17 04:19:10 +0500

    refactor: wrap results summary component in a main tag for improved semantic structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,11 @@ import SummaryCard from './ui/SummaryCard/SummaryCard';
 
 export default function App() {
   return (
-    <div className="results-summary-component">
-      <FinalScoreCard />
-      <SummaryCard />
-    </div>
+    <main>
+      <div className="results-summary-component">
+        <FinalScoreCard />
+        <SummaryCard />
+      </div>
+    </main>
   );
 }
